31c316279bb08cb8d481d5680c0c78c79864c979
unknown <lbelliski@gmail.com>
2019-11-19 23:55:21 -0300

    primer commit

---
+++ clase-38.js
@@ -0,0 +1,88 @@
+// let dia = prompt(`Por favor elija un día de la semana`);
+
+// switch (dia) {
+//     case "lunes":
+//         alert("Su nombre proviene de la luna. En latín, se lo llamaba 'dies Lūnae'")
+
+//         break;
+//     case "martes":
+//         alert("Su nombre proviene del planeta Marte. En latín, se lo llamaba 'dies Martis'")
+
+//         break;
+//     case "miercoles":
+//         alert("Su nombre proviene del planeta Mercurio. En latín, se lo llamaba 'dies Mercuriī'")
+
+//         break;
+//     case "jueves":
+//         alert("Su nombre proviene de Jupiter")
+
+//         break;
+//     case "viernes":
+//         alert("Su nombre proviene del planeta Venus. En latín, se lo llamaba 'dies Veneris'")
+
+//         break;
+//     case "sabado":
+//         alert("Su nombre proviene del Shabat hebreo, el día de descanso del pueblo judío")
+
+//         break;
+//     case "domingo":
+//         alert("Su nombre es cristiano y proviene de la palabra latina 'Dominica', que significa 'señoril', por ser 'el día del Señor'")
+
+//         break;
+//     default:
+//         alert("No ha elegido una opción válida")
+//         break;
+// }
+
+// let mes = prompt("Ingrese el número correspondiente al mes elegido")
+// mes = Number(mes)
+// mesLargo = 31
+// mesCorto = 30
+// mesUnico = 28 + " o 29 en años bisiestos"
+
+// switch (mes) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         alert(`El mes tiene 31 días`)
+//         break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         alert("El mes tiene 30 días")
+//         break;
+//     case 2:
+//         alert("El mes tiene 28 días, 29 en años bisiestos")
+
+//     default:
+//         alert("No ha ingresado un dato válido")
+//         break;
+// }
+
+// for(let i = 0; i < 101; i++) {console.log (i)}
+// for(let i = 0; i<11; i++) { console.log (i)}
+// for(let i = 100; i>=0; i--) {console.log (i)}
+// punto 6
+// for(let i = 0; i < 91; i+=9) {console.log (i)}
+// for (let i = 1; i <=90; i*=9) {console.log (i)}
+//punto 7
+// let sumaParcial = 0;
+// for(let i = 0; i <= 1000; i ++) {
+//     console.log (sumaParcial = sumaParcial + i)
+// };
+//punto 8 
+// let sumaParcial2 = 0;
+// for(let i = 0; i <= 1000; i++) {
+//     if (i % 2 !=0)
+//     {console.log (sumaParcial2 = sumaParcial2 + i)
+// }};
+// punto 9
+// let dibujo = ""
+// for (let i = 0; i < 14; i++){
+//     console.log (dibujo = `*` + dibujo)
+// };
